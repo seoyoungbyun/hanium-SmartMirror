4b4b9bea7680b1626b29edd047b97a79688b9e4b
mhyeon-kim <80239994+mhyeon-kim@users.noreply.github.com>
2024-11-06 17:19:10 +0900

    Add files via upload

---
+++ MagicMirror/modules/MMM-Info/MMM-Info.js
@@ -0,0 +1,37 @@
+Module.register("MMM-Info", {
+    defaults: {
+        updateInterval: 5000, // 업데이트 주기 (밀리초 단위)
+        filePath: "/home/mirror/information.txt" // 텍스트 파일 경로
+    },
+
+    start: function() {
+        this.content = "Loading...";
+        this.getData();
+        this.scheduleUpdate();
+    },
+
+    getData: function() {
+        this.sendSocketNotification("GET_TEXT", this.config.filePath);
+    },
+
+    socketNotificationReceived: function(notification, payload) {
+        if (notification === "TEXT_CONTENT") {
+            this.content = payload;
+            this.updateDom();
+        }
+    },
+
+    scheduleUpdate: function() {
+        var self = this;
+        setInterval(function() {
+            self.getData();
+        }, this.config.updateInterval);
+    },
+
+    getDom: function() {
+        var wrapper = document.createElement("div");
+        wrapper.className = "MMM-TextDisplay";
+        wrapper.innerHTML = this.content.replace(/\n/g, "<br>");
+        return wrapper;
+    }
+});
